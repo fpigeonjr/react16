20acb4a5ba31488b889e15776b7f66671f002409
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-11-10 09:24:39 -0600

    adds componentDidCatch to handle errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import StudentForm from './components/StudentForm';
+import StudentForm from "./components/StudentForm";
 
 class App extends Component {
-  
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+  state = {
+    hasError: false
+  };
 
-        <StudentForm />
-      </div>
-    );
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong!</h1>;
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+          <StudentForm />
+        </div>
+      );
+    }
   }
 }
 
