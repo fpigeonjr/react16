f7c12846dc4722b404d0d6718864a9fd2fd2dbdd
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-11-10 10:42:31 -0600

    adds ErrorBoundary component to catch errors in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 import StudentForm from "./components/StudentForm";
 
@@ -16,20 +17,17 @@ class App extends Component {
   }
 
   render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong!</h1>;
-    } else {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <ErrorBoundary>
           <StudentForm />
-        </div>
-      );
-    }
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
