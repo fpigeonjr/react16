acdf07f827aaf3bebc2a3419fa55b8c990ab2eab
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-11-10 21:01:20 -0600

    renders children into other Dom Nodes with portals

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Modal from "./components/Modal";
 
-import StudentForm from './components/StudentForm';
+import StudentForm from "./components/StudentForm";
 
 class App extends Component {
+  handleClick = () => {
+    console.log("I was clicked");
+  };
+
   render() {
     return (
-      <div className="App">
+      <div className="App" onClick={this.handleClick}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <StudentForm />
+        <Modal>
+          <div className="modal">
+            I AM A MODAL<button>Close x</button>
+          </div>
+        </Modal>
       </div>
     );
   }
