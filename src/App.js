136c0c9e420b93bf5503010794c0ce20f59d53f5
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-11-10 21:03:15 -0600

    start of return null from setState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Modal from "./components/Modal";
+import React, { Component } from 'react';
 
-import StudentForm from "./components/StudentForm";
+import Teacher from './Teacher';
 
 class App extends Component {
-  handleClick = () => {
-    console.log("I was clicked");
+
+  state = {
+    teacher: ''
   };
 
+  updateTeacher = teacher => {
+    this.setState({ teacher });
+  }
+
   render() {
+    const teachers = ['jay', 'vivianne', 'ecma', 'json'];
+
     return (
-      <div className="App" onClick={this.handleClick}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="App">
+        <header>
+          <h1>Meet the Teachers</h1>
+          {teachers.map((teacher,i) =>
+            <button
+              key={teacher}
+              type="button"
+              value={teacher}
+              onClick={e => this.updateTeacher(e.target.value)}
+            >{teacher}
+            </button>)}
         </header>
-        <StudentForm />
-        <Modal>
-          <div className="modal">
-            I AM A MODAL<button>Close x</button>
-          </div>
-        </Modal>
+
+        <Teacher teacher={this.state.teacher} />
       </div>
     );
   }
